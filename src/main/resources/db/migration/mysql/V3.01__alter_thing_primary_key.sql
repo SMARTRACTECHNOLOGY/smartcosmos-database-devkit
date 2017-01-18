eba9d7119c0ec3aae60d1ebd9de3160e12fcf92f
Will Schneider <will.schneider@smartrac-group.com>
2017-01-18 16:21:23 -0500

    OBJECTS-1196: change PK from id to tenantId and id

---
+++ src/main/resources/db/migration/mysql/V3.01__alter_thing_primary_key.sql
@@ -0,0 +1,27 @@
+-- Update primary key to be id within tenantId
+--
+-- disable auto-commit for DDL updates so we don't have to jump through hoops if it fails.
+-- see https://flywaydb.org/documentation/faq.html#repair and https://mariadb.com/kb/en/mariadb/start-transaction/
+SET AUTOCOMMIT = 0
+;
+
+START TRANSACTION
+;
+
+ALTER TABLE thing
+  DROP INDEX IF EXISTS UK_51ftm4jj6o6r8i4rxlv35mjdo
+;
+
+ALTER TABLE thing
+  DROP PRIMARY KEY
+;
+
+ALTER TABLE thing
+  ADD PRIMARY KEY (tenantId, id)
+;
+
+COMMIT
+;
+
+SET AUTOCOMMIT = 1
+;
